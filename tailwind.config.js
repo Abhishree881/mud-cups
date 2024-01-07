6fa07fbeac9cf0d532cd1fce3c9f9bf390aa757c
Abhishree21 <abhishree.a@superbolter.com>
2024-01-07 12:59:28 +0530

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/index.js",
-    "./src/App.js",
-    "./src/pages/admin.js",
-  ],
+  content: ["./src/index.js", "./src/App.js", "./src/pages/Admin.js"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
