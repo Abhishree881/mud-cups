c9843dbd4a6ba631f1fb5211a4c9e39147d71cae
Abhishree881 <abhishree881@gmail.com>
2024-01-10 22:17:42 +0530

    changed category page

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -1,31 +1,38 @@
-import React from 'react'
-import CategoryCard from '../components/categoryCard'
+import React from "react";
+import CategoryCard from "../components/categoryCard";
 import { FaPlus } from "react-icons/fa6";
+import "../assets/styles/category.css";
 
 function Category() {
   const data = [
     {
       index: 1,
-      name: 'North Indian',
+      name: "North Indian",
       len: 12,
     },
     {
       index: 2,
-      name: 'Punjabi',
+      name: "Punjabi",
       len: 1,
     },
     {
       index: 3,
-      name: 'Russian',
+      name: "Russian",
       len: 69,
-    }
-  ]
+    },
+    {
+      index: 4,
+      name: "South Indian",
+      len: 30,
+    },
+  ];
   return (
-    <div className='relative w-[100vw] min-h-[100vh] py-4' style={{ background: "linear-gradient(#fce0b9, #f5ce8a)" }}>
-      <div className='font-bold text-3xl pl-4 pb-2'>All Categories</div>
-      <div className='flex flex-col gap-[10px] px-4 py-2 h-auto mb-6'>
+    <div className="category relative w-[100vw] min-h-[100vh] py-4 px-2">
+      <div className="category-header">Create Category</div>
+      <div className="category-sub-header px-[16px]">All Categories</div>
+      <div className="flex flex-col gap-[10px] px-4 py-2 h-auto mb-6">
         {data.map((index) => {
-          return <CategoryCard data={index} key={index.index} />
+          return <CategoryCard data={index} key={index.index} />;
         })}
       </div>
       {/* <div className='fixed z-[100] bottom-[20px] right-[20px]'>
@@ -34,7 +41,7 @@ function Category() {
         </div>
       </div> */}
     </div>
-  )
+  );
 }
 
-export default Category
+export default Category;
