e51cd77803248d4763020dc4b2e2e0935719fe2d
Anmol376OP <anmolsrivastav7102@gmail.com>
2024-01-10 18:57:16 +0530

    small update to card

--- src/components/itemCardSmall.jsx
+++ src/components/itemCardSmall.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaRegHeart } from "react-icons/fa";
+import { FaHeart } from "react-icons/fa6";
 
 function ItemCardSmall(props) {
     return (
         <div className='min-w-[130px] h-full flex flex-col gap-[6px] relative'>
             <div className='absolute w-fit top-[6px] left-[-5px] text-white bg-[#a2630b] text-[12px] leading-[14px] rounded-[6px] font-[400] px-[6px] py-[3px]'>{props.data.category}</div>
+            <div className='absolute top-[9px] right-[5px]'>{props.data.index % 2 != 0 ? <FaRegHeart color='white' fontSize={'14px'} /> : <FaHeart color='#eb1727' fontSize={'14px'} />} </div>
             <div className='flex-[75] rounded-[8px]' style={{ backgroundImage: `url(${props.data.imgUrl})`, backgroundSize: '100% 100%', backgroundPosition: 'center' }}></div>
             <div className='flex-[25] flex flex-col gap-[6px]'>
                 <div className='text-[15px] leading-[14px] font-[600]'>{props.data.name}</div>
