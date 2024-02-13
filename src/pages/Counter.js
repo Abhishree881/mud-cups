043268a27ef9059198e84d5849ccd4fd569f47cf
Abhishree881 <abhishree881@gmail.com>
2024-02-13 12:39:17 +0530

    fixed issues

--- src/pages/Counter.js
+++ src/pages/Counter.js
@@ -16,23 +16,35 @@ function Counter() {
     };
   }, [id]);
   return (
-    <div>
+    <div style={{ width: "98%", margin: "auto", padding: "10px" }}>
       <div className="font-bold text-2xl">Counter : {id}</div>
       {data &&
         data.map((item, index) => {
           return (
-            <div>
+            <div
+              style={{
+                border: "1px dashed rgb(0,0,0,0.5)",
+                width: "99%",
+                margin: "auto",
+                padding: "10px",
+                margin: "10px 0",
+              }}
+            >
               <span>
                 {"Order number: "}
                 {index + 1}
               </span>
               <span>
-                {" "}
+                {" and item is "}
                 {item.count}
                 {" * "}
                 {item.itemName}
               </span>
-              <span> {item.tableNumber}</span>
+              <span>
+                {" "}
+                {"on table: "}
+                {item.tableNumber}
+              </span>
             </div>
           );
         })}
